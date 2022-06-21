31296253c83d30a433036030d96004dc50afe7a7
ACuteWoof <sutharshanvithushan@gmail.com>
2022-06-21 21:44:08 +0530

    rm ltd

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="footer footer-center p-5 bg-cyan-900 text-primary-content">
   <div>
     <p class="font-bold text-slate-300">
-      Woof OS organization Ltd. <br />Providing you with Woof OS since 2021
+      Woof OS<br />Providing you with Woof OS since 2021
     </p>
     <p class="text-slate-300">
       Copyright © {copyrightYear} - Christian Llupo and WoofOS team. All rights reserved.
