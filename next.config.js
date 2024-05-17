6e0738e31a41ce52a2a6fe21f17bc2f8575b7242
ACuteWoof <sutharshanvithushan@gmail.com>
2024-05-17 12:20:29 +0530

    export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: "out",
   images: {
     remotePatterns: [
+      unoptimized: true,
       {
         protocol: "https",
         hostname: "raw.githubusercontent.com",
