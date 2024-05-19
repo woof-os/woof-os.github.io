1f25e5fa49d703eb53ce6f8fae716d440e7fe2fd
ACuteWoof <sutharshanvithushan@gmail.com>
2024-05-19 16:30:18 +0530

    keybinds

--- app/sections/contentpages/keybinds.tsx
+++ app/sections/contentpages/keybinds.tsx
@@ -29,6 +29,7 @@ export default function Keybinds() {
                 <li><kbd>[mod]</kbd> <kbd>J</kbd> moves focus <b>down</b></li>
                 <li><kbd>[mod]</kbd> <kbd>K</kbd> moves focus <b>up</b></li>
                 <li><kbd>[mod]</kbd> <kbd>Space</kbd> moves focus to the <b>next window</b></li>
+                <li><kbd>[mod]</kbd> <kbd>.</kbd> moves focus to the <b>next screen</b></li>
                 <p>
                     The mouse can also be used to switch focus between windows by simply hovering over them.
                 </p>
@@ -40,10 +41,10 @@ export default function Keybinds() {
         <div>
             <h2>Window arrangement</h2>
             <ul>
-                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>H</kbd> moves focus to the <b>left</b></li>
-                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>L</kbd> moves focus to the <b>right</b></li>
-                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>J</kbd> moves focus <b>down</b></li>
-                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>K</kbd> moves focus <b>up</b></li>
+                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>H</kbd> moves focused window to the <b>left</b></li>
+                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>L</kbd> moves focused window to the <b>right</b></li>
+                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>J</kbd> moves focused window <b>down</b></li>
+                <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>K</kbd> moves focused window <b>up</b></li>
                 <li><kbd>[mod]</kbd> <kbd>Shift</kbd> <kbd>Return</kbd> toggles between split and unsplit sides of stack</li>
                 <li><kbd>[mod]</kbd> <kbd>Tab</kbd> toggles between layouts</li>
             </ul>
@@ -61,12 +62,14 @@ export default function Keybinds() {
         <div>
             <h2>Miscellaneous and launching programs</h2>
             <ul>
-                <li><kbd>[mod]</kbd> <kbd>Return</kbd> launches the terminal</li>
+                <li><kbd>[mod]</kbd> <kbd>Return</kbd> launches the terminal (alacritty)</li>
                 <li><kbd>[mod]</kbd> <kbd>R</kbd> opens the Rofi run launcher</li>
                 <li><kbd>[mod]</kbd> <kbd>B</kbd> launches the web browser (brave)</li>
-                <li><kbd>[mod]</kbd> <kbd>C</kbd> launches the file manager (pcmanfm)</li>
+                <li><kbd>[mod]</kbd> <kbd>C</kbd> launches the file manager (vifm)</li>
+                <li><kbd>[mod]</kbd> <kbd>Z</kbd> opens copied PDF URL in Zathura</li>
+                <li><kbd>[mod]</kbd> <kbd>Y</kbd> opens copied YouTube URL in MPV (using yt-dlp)</li>
                 <li><kbd>[mod]</kbd> <kbd>F</kbd> opens the Flameshot GUI</li>
-                <li><kbd>[mod]</kbd> <kbd>S</kbd> takes a full screenshot using scrot</li>
+                <li><kbd>[mod]</kbd> <kbd>S</kbd> takes a full screenshot using Scrot</li>
                 <li><kbd>[mod]</kbd> <kbd>T</kbd> spawns a command using a prompt</li>
                 <li><kbd>[mod]</kbd> <kbd>V</kbd> shows the active windows through Rofi</li>
                 <li><kbd>[mod]</kbd> <kbd>D</kbd> sends a notification including the current date and time</li>
