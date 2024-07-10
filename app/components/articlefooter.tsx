afdff9ba639d4808476aa5eb58ebc35708246ee6
ACuteWoof <sutharshanvithushan@gmail.com>
2024-07-11 00:26:15 +0530

    rm matrix

--- app/components/articlefooter.tsx
+++ app/components/articlefooter.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
 
 function ArticleFooter() {
-    return (
-        <div className="max-w-screen-2xl flex-grow p-4 lg:p-12 lg:bg-slate-900 text-slate-200">
-            <div
-                className="prose max-w-none prose-slate prose-invert md:px-12 md:grid md:grid-cols-2">
-                <section id="contact">
-                    <h2>Contact</h2>
-                    <ul>
-                        <li>
-                            Email:&nbsp;
-                            <Link target="_blank" href="mailto:sutharshanvithushan@gmail.com">
-                                sutharshanvithushan@gmail.com
-                            </Link>
-                        </li>
-                        <li>
+  return (
+    <div className="max-w-screen-2xl flex-grow p-4 lg:p-12 lg:bg-slate-900 text-slate-200">
+      <div className="prose max-w-none prose-slate prose-invert md:px-12 md:grid md:grid-cols-2">
+        <section id="contact">
+          <h2>Contact</h2>
+          <ul>
+            <li>
+              Email:&nbsp;
+              <Link target="_blank" href="mailto:sutharshanvithushan@gmail.com">
+                sutharshanvithushan@gmail.com
+              </Link>
+            </li>
+            {/* <li>
                             Matrix:&nbsp;
                             <Link target="_blank" href="https://matrix.to/#/#woof-os:matrix.org">
                                 #woof-os:mx.lewoof.xyz
@@ -23,29 +22,35 @@ function ArticleFooter() {
                                 @acutewoof:mx.lewoof.xyz
                             </Link>
                         </li>
-                        <li>
-                            Discord:&nbsp;
-                            <Link target="_blank" href="https://discord.gg/2G2yGUAXUS">
-                                Woof OS
-                            </Link>
-                        </li>
-                    </ul></section>
-                <section id="donate">
-                    <h2>Donate</h2>
-                    <ul>
-                        <li>
-                            <strong>Monero: </strong>
-                            <span className="text-wrap break-all">88Ftpu3ncRyLv7Z2c14b9L4MsyWscEqLBJhENxviYtmtRfidDx6G9y3Xyd8aYF8NRV2c2dF5QZiyTfHwuWLH9qmsSpskLhj</span>
-                        </li>
-                        <li>
-                            <strong>BuyMeACoffee: </strong>
-                            <Link target="_blank" href="https://buymeacoffee.com/acutewoof">acutewoof</Link>
-                        </li>
-                    </ul>
-                </section>
-            </div>
-        </div>
-    );
+			*/}
+            <li>
+              Discord:&nbsp;
+              <Link target="_blank" href="https://discord.gg/2G2yGUAXUS">
+                Woof OS
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <section id="donate">
+          <h2>Donate</h2>
+          <ul>
+            <li>
+              <strong>Monero: </strong>
+              <span className="text-wrap break-all">
+                88Ftpu3ncRyLv7Z2c14b9L4MsyWscEqLBJhENxviYtmtRfidDx6G9y3Xyd8aYF8NRV2c2dF5QZiyTfHwuWLH9qmsSpskLhj
+              </span>
+            </li>
+            <li>
+              <strong>BuyMeACoffee: </strong>
+              <Link target="_blank" href="https://buymeacoffee.com/acutewoof">
+                acutewoof
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default ArticleFooter;
