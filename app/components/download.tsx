609e32954382a16b6836cdce754e79c6782201b2
ACuteWoof <sutharshanvithushan@gmail.com>
2024-07-11 00:27:43 +0530

    mark mirrors offline

--- app/components/download.tsx
+++ app/components/download.tsx
@@ -1,23 +1,44 @@
 import Link from "next/link";
 
 export default function Download({ editionId }: { editionId: string }) {
-    return <section id="download">
-        <h1>Download</h1>
-        <span className="flex flex-col gap-2 md:flex-row md:gap-12">
-            <span>
-                <h2>LeWoof Mirror</h2>
-                <Link target="_blank" href={`https://dl.os.lewoof.xyz/${editionId}/woofos${editionId === "qtile" ? "" : `-${editionId}`
-                    }-x86_64.iso`}>Latest release</Link>
-                <br />
-                <Link target="_blank" href={`https://dl.os.lewoof.xyz/${editionId}/`}>Browse downloads</Link>
-            </span>
-            <span>
-                <h2>Github Mirror</h2>
-                <Link target="_blank" href={`https://github.com/woof-os/isos-${editionId}/releases/latest/download/woofos${editionId === "qtile" ? "" : `-${editionId}`
-                    }.iso`}>Latest release</Link>
-                <br />
-                <Link target="_blank" href={`https://github.com/woof-os/isos-${editionId}/releases`}>Browse downloads</Link>
-            </span>
+  return (
+    <section id="download">
+      <h1>Download</h1>
+      <span className="flex flex-col gap-2 md:flex-row md:gap-12">
+        <span>
+          <h2>LeWoof Mirror (Temporarily Offline)</h2>
+          <Link
+            target="_blank"
+            href={`https://dl.os.lewoof.xyz/${editionId}/woofos${
+              editionId === "qtile" ? "" : `-${editionId}`
+            }-x86_64.iso`}
+          >
+            Latest release
+          </Link>
+          <br />
+          <Link target="_blank" href={`https://dl.os.lewoof.xyz/${editionId}/`}>
+            Browse downloads
+          </Link>
         </span>
+        <span>
+          <h2>Github Mirror</h2>
+          <Link
+            target="_blank"
+            href={`https://github.com/woof-os/isos-${editionId}/releases/latest/download/woofos${
+              editionId === "qtile" ? "" : `-${editionId}`
+            }.iso`}
+          >
+            Latest release
+          </Link>
+          <br />
+          <Link
+            target="_blank"
+            href={`https://github.com/woof-os/isos-${editionId}/releases`}
+          >
+            Browse downloads
+          </Link>
+        </span>
+      </span>
     </section>
+  );
 }
